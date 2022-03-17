2fe9a2941633d1a0ba6893f6380f776c196d2db1
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-17 18:59:38 -0300

    feat: add file with global colors and breakpoints

---
+++ src/theme/index.js
@@ -0,0 +1,17 @@
+export default {
+  breakpoints: {
+    xsm: '360px',
+    sm: '480px',
+    md: '1024px',
+    lg: '1440px',
+  },
+  colors: {
+    primary: '#00A870',
+    secondary: '#5E20A4',
+    background: '#FAF9F7',
+    grey0: '#000000',
+    grey24: '#3D3D3D',
+    grey88: '#DFDFDF',
+    white: '#FFFFFF',
+  },
+};
