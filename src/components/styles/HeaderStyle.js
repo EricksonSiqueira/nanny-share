d7e9114e79f3c53b95d044c2b86fb1c98e545f0b
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-17 20:07:29 -0300

    style: add styles to header

---
+++ src/components/styles/HeaderStyle.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+import theme from '../../theme';
+
+export const MobileHeader = styled.header`
+  width: 100%;
+  height: 64px;
+
+  img.badge-img {
+    position: absolute;
+    top: 0;
+    left: 16px;
+  }
+
+  button {
+    position: absolute;
+    right: 16px;
+    top: 16px;
+    background-color: ${theme.colors.primary};
+    color: ${theme.colors.white};
+    width: 234px;
+    height: 44px;
+    border: none;
+    border-radius: 4px;
+  }
+
+  button h5, a h5 {
+    color: ${theme.colors.white};
+  }
+`;
+
+export const DesktopHeader = styled.header`
+  background-color: tomato;
+  width: 100%;
+  height: 72px;
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  nav {
+    display: flex;
+    gap: 32px;
+    margin-left: 108px;
+  }
+
+  section {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 23px;
+    margin-right: 32px;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.colors.white}
+  }
+
+  img.badge-img {
+    position: absolute;
+    top: 0;
+    left: 20px;
+  }
+  button {
+    background-color: ${theme.colors.primary};
+    width: 234px;
+    height: 44px;
+    border: none;
+    border-radius: 4px;
+  }
+  h5 {
+    color: ${theme.colors.white};
+  }
+`;
