0a003fa1103af3dced1f46fa39dc333735d23b31
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-17 20:09:52 -0300

    refactor: remove test stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import Header from './components/Header';
+import LandingPage from './pages/LandingPage';
 import AppWrapper from './AppStyle';
+import './App.css';
 
 function App() {
   return (
     <AppWrapper>
-      <Header />
-      <h1>Teste h1</h1>
-      <h2>Share your home,nanny and costs</h2>
-      <h3>Teste h3</h3>
-      <a href="https://github.com/EricksonSiqueira">Sarah’s day care available now in North Sydney</a>
+      <LandingPage />
     </AppWrapper>
   );
 }
