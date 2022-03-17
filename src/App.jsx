f78a7e561037aaffa1fb781f42d2c496f147a5fe
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-17 17:27:39 -0300

    fix: change .js to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import React from 'react';
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <p>Clean</p>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MobileHeader } from './styles/HeaderStyle';
+
+function Header() {
+  return (
+    <MobileHeader>
+      <p>Header</p>
+    </MobileHeader>
+  );
+}
+
+export default Header;
