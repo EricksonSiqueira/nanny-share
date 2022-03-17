22822d678e3e0fe00656f70be6cd166f0f4f5876
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-17 19:34:40 -0300

    fix: reset CSS correctly

--- src/AppStyle.js
+++ src/AppStyle.js
@@ -5,9 +5,6 @@ export default styled.div`
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap');
 
   * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
     font-family: 'Inter', sans-serif;
     color: ${theme.colors.grey24};
     font-style: normal;
