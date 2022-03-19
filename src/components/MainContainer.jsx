e6d5212e35f74ca1fa571894da2d9f07f0d6f58b
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-18 23:22:31 -0300

    feat: add share nanny section to main

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Hero from './Hero';
 import AvailableNanny from './AvailableNanny';
+import ShareNanny from './ShareNanny';
+import useMediaQuery from '../hooks/useMediaQuery';
+import theme from '../theme/index';
 
 function MainContainer() {
+  const isLarge = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
   return (
     <main>
       <Hero />
       <AvailableNanny />
+      <ShareNanny isLarge={isLarge} />
     </main>
   );
 }
