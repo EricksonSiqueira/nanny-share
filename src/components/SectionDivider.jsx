fe78cff8a7969232007b361e9c2234c9c07bdedf
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-18 23:56:46 -0300

    feat: SectionDivider component

---
+++ src/components/SectionDivider.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import StyledSectionDivider from './styles/SectionDivider';
+
+function SectionDivider() {
+  return (
+    <StyledSectionDivider />
+  );
+}
+
+export default SectionDivider;
