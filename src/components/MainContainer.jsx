242ce978af0398d7845378b60a6d3ff29b95b989
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-19 19:22:01 -0300

    fix: overflow bug

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import StyledMainContainer from './styles/MainContainerStyle';
 import Hero from './Hero';
 import AvailableNanny from './AvailableNanny';
 import ShareNanny from './ShareNanny';
 import LookingForNannyForm from './LookingForNannyForm';
-import SectionDivider from './styles/SectionDivider';
+import SectionDivider from './SectionDivider';
 import useMediaQuery from '../hooks/useMediaQuery';
 import theme from '../theme/index';
 
 function MainContainer() {
   const isLarge = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
   return (
-    <main>
+    <StyledMainContainer>
       <Hero isLarge={isLarge} />
       <AvailableNanny isLarge={isLarge} />
       <ShareNanny isLarge={isLarge} />
       <SectionDivider />
       <LookingForNannyForm isLarge={isLarge} />
       <SectionDivider />
-    </main>
+    </StyledMainContainer>
   );
 }
 
