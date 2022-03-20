b0534feaf476990d460310d7a3441fc41a050d27
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-20 16:50:09 -0300

    feat: add Footer to landing page component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import StyledLandingPage from './styles/LandingPageStyle';
 import MainContainer from '../components/MainContainer';
+import Footer from '../components/Footer';
 
 function LandingPage() {
   return (
     <StyledLandingPage>
       <Header />
       <MainContainer />
+      <Footer />
     </StyledLandingPage>
   );
 }
